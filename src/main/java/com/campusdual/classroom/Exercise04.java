b7dad608cc98077678908a8efd865534c2aa735b
Juanfer <juanfer112@hotmail.com>
2024-11-24 17:10:25 +0100

    feature: implemented 4 method sumNum, sumEventNum, factorial, recursiveFactorial

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -9,22 +9,59 @@ public class Exercise04 {
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
-
+        System.out.println("Metodo Suma de los primeros 5 números: " + sumNum(5));
+        System.out.println("Suma de los primeros 20 números pares: " + sumEvenNum(20));
+        System.out.println("Obtén el factorial del número 7 → (7!): " + factorial(7));
+        System.out.println("Obtén el recursive factorial del número 7 → (7!): " + recursiveFactorial(1));
     }
 
+    /**
+     * Calcula la suma de los primeros N números.
+     *
+     * @param num Número hasta el cual sumar.
+     * @return La suma de los números.
+     */
     public static int sumNum(int num) {
-        return 0;
+        if(num<=0) return 0;
+        return num*(num + 1)/2;
     }
 
+    /**
+     * Calcula la suma de los primeros N números pares.
+     *
+     * @param num Número de pares a sumar.
+     * @return La suma de los números pares.
+     */
     public static int sumEvenNum(int num) {
-        return 0;
+        if (num <= 0) return 0;
+        return num * (num + 1);
     }
 
+    /**
+     * Calcula el factorial de un número usando un bucle.
+     *
+     * @param num Número para calcular el factorial.
+     * @return El factorial del número.
+     */
     public static int factorial(int num) {
-        return 0;
+        if (num < 0) throw new IllegalArgumentException("El factorial no está definido para números negativos.");
+        int result = 1;
+        if(num > 1) {
+            for(int i = 1; i <= num; i++){
+                result= result*i;
+            }
+        }
+        return result;
     }
 
+    /**
+     * Calcula el factorial de un número de forma recursiva.
+     *
+     * @param num Número para calcular el factorial.
+     * @return El factorial del número.
+     */
     public static int recursiveFactorial(int num) {
-        return 0;
+        if (num < 0) throw new IllegalArgumentException("El factorial no está definido para números negativos.");
+        return  (num <= 1) ? 1 : num * recursiveFactorial(num - 1);
     }
 }
